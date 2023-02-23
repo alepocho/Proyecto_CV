a8bfb81c4e4bf7c4dd189f1b14adcb2bb907bc87
Alejandro Musso <alegmusso@gmail.com>
2023-02-23 14:29:31 -0300

    agregue js y css

--- CV_FULL_RESPONSIVE/cv_jscript.js
+++ CV_FULL_RESPONSIVE/cv_jscript.js
@@ -1,7 +1,29 @@
-document.getElementById ("boton")
 
-document.getElementById ('boton_js').onclick = function(){
- document.getElementById('cosa').innerHTML = "cambiar";
-  
+
+  document.getElementById('boton_html').onclick = function (){
+    var x = document.getElementById('info_html');
+     if (x.style.display === "block") {
+       x.style.display = "none";
+     } else {
+       x.style.display = "block";
+     }
 
 };
+document.getElementById('boton_css').onclick = function (){
+    var x = document.getElementById('info_css');
+     if (x.style.display === "block") {
+       x.style.display = "none";
+     } else {
+       x.style.display = "block";
+     }
+};
+
+document.getElementById('boton_js').onclick = function (){
+    var x = document.getElementById('info_js');
+     if (x.style.display === "block") {
+       x.style.display = "none";
+     } else {
+       x.style.display = "block";
+     }
+
+    };
